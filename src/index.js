f542cddd4f517cb036e0a072a1426811a71bab6e
hiba-elkhazraji <hiponita08@gmail.com>
2025-09-24 14:25:12 +0100

    added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("HELLOOOOOOOOO HIBAAAA");
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: ["la tombe dit a la rose"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
